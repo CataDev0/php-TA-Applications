3b7783103c968689467131fc86ec7cccfd2d0623
Cata <ole.kh@hotmail.com>
2025-10-08 15:15:18 +0200

    Adds basic login functionality
    Implements a basic login form.
    Updates the README with installation instructions.
    Updates dependencies.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<form>
+    <h2>Login</h2>
+    <h3>Brukernavn</h3>
+    <input name="loginUsername" required type="text" value=""/>
+    <h3>Passord</h3>
+    <input name="loginPassword" required type="password" value=""/>
+</form>
