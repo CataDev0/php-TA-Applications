ba622cd7fcf036342000358a7f98ec61762378cd
Cata <cata@cata.moe>
2025-10-19 16:06:38 +0200

    Adds comments for register route.
    Adds comments to indicate a register route will be implemented.
    Also clarifies the register button should only display when the user is not logged in.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,6 +26,8 @@
                                 Log in
                             </a>
 
+                            {{-- TBD Register route --}}
+                            {{-- Only show register button if user is not logged in --}}
                             @if (Route::has('register'))
                                 <a href="{{ route('register') }}"
                                    class="inline-flex items-center px-4 py-2 bg-white border-gray-300  rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">
